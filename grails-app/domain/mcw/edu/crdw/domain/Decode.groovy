d10b6a9ac6d09d227a826d6fd5cd5e104fae1060
George Kowalski <gkowalski@mcw.edu>
2012-08-28 14:24:36 -0500

    Changed default message to show decode of various fields.

--- grails-app/domain/mcw/edu/crdw/domain/Decode.groovy
+++ grails-app/domain/mcw/edu/crdw/domain/Decode.groovy
@@ -2,7 +2,10 @@ package mcw.edu.crdw.domain
 
 class Decode {
 
-    String message = "Type your message to de de-identified here";
+    String message = "Type your message to de de-identified here  \n\
+Dr Unknown on 08/08/2012 \n\
+From Milwaukee, WI \n\
+Calling from 414-555-5555";
     static constraints = {
         
     }
